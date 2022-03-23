a99d32877c3d8f0eb339a1503cc7a9824ec33630
Janne Peippo <peippo@gmail.com>
2022-03-23 18:44:29 +0200

    Add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const newGameRoute = require("./routes/newGameRoute.js");
 const flipTileRoute = require("./routes/flipTileRoute.js");
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
 	res.json({ message: "alive" });
