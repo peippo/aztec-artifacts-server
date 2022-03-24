1cc75d9a6a9d49a8f903d134fa3ff125d028a6b7
Janne Peippo <peippo@gmail.com>
2022-03-24 19:17:06 +0200

    Adjust flip tile route to return id and symbol

--- src/services/flipTile.js
+++ src/services/flipTile.js
@@ -3,15 +3,24 @@ const gamesList = require("./games");
 const flipTile = (query) => {
 	let { gameId, tileId } = query;
 
+	if (!gamesList[gameId]) {
+		throw "Game not found!";
+	}
+	console.log(tileId);
 	const isFirst = typeof tileId === "string";
 	let revealedTiles = [];
 	let isMatch = false;
 
 	if (isFirst) {
-		revealedTiles = [gamesList[gameId]["tiles"][tileId], undefined];
+		revealedTiles = [
+			{ id: parseInt(tileId), symbol: gamesList[gameId]["tiles"][tileId] },
+		];
 	} else {
-		revealedTiles = tileId.map((id) => gamesList[gameId]["tiles"][id]);
-		isMatch = revealedTiles[0] === revealedTiles[1];
+		revealedTiles = tileId.map((id) => ({
+			id: parseInt(id),
+			symbol: gamesList[gameId]["tiles"][id],
+		}));
+		isMatch = revealedTiles[0]["symbol"] === revealedTiles[1]["symbol"];
 	}
 
 	return { revealedTiles, isFirst, isMatch };
